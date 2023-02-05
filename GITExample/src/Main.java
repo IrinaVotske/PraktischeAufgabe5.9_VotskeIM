d9b8131589612fb1d6d9f4dd0c98204db4c2246e
Irina Votske <irina.votske@hotmail.com>
2023-02-05 08:50:39 +0300

    Commit4 test

--- GITExample/src/Main.java
+++ GITExample/src/Main.java
@@ -4,6 +4,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MMMM/dd");
+        System.out.println("test");
         System.out.println(formatter.format(new Date()));
     }
 }
