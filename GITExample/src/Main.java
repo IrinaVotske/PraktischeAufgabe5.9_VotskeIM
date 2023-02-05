cf2a73ef3cea3f23bd89703b9f63afa2ec0c1b1f
Irina Votske <irina.votske@hotmail.com>
2023-02-05 08:44:59 +0300

    Commit3 DateFormat_MMMM

--- GITExample/src/Main.java
+++ GITExample/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MMMM/dd");
         System.out.println(formatter.format(new Date()));
     }
 }
